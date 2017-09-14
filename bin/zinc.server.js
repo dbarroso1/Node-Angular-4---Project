8d2b1a5587a30b8202ddedf41793cc9ac0a351a3
Dilean Barroso <dileanbarroso@gmail.com>
2017-09-14 03:43:27 -0400

    Update zinc.server.js

--- bin/zinc.server.js
+++ bin/zinc.server.js
@@ -4,9 +4,10 @@ const zn = require('./zinc.config'), log = require('./zinc.logger'), api = requi
 // Start Connection to Database
 mongoose.Promise = require('bluebird')
 mongoose.connect(zn.databaseURI, { useMongoClient: true })
-mongoose.connection.on('open', function () { log.dbCheck() }) //Mongoose Error Handling
-    .on('error', () => { console.log("Mongo Error: ") })
-    .on('disconnected', () => { console.log("MongoDB Disconnected! EXITING . . ."); process.exit(0) })
+//Mongoose Error Handling
+mongoose.connection.on('open', (err) => { log.dbCheck() })
+.on('error', (err) => { log.errMgmt(err); process.exit(1)})
+.on('disconnected', (err) => { log.errFatal(err); process.exit(0) })
 
 zinc.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }))//Express JSON Body Parser
 zinc.use(express.static(path.join(__dirname, '../public'))); // Path for Angular 
@@ -16,10 +17,7 @@ zinc.get('*', function (err, req, res, next) {
     res.sendfile(path.join(__dirname, 'index.html'))
 })
 zinc.use('/api', api) // Api Starts Here
-zinc.use(function (err, req, res, next) { 
-    log.errMgmt(err)  // Error handling middleware
-    res.send(err)
-})
+zinc.use(function (err, req, res, next) { res.send(err); log.errMgmt(err) })
 
 // Zinc Server Start
 zinc.listen(zn.svport, zn.svhost, function () {
