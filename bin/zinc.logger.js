f5184409f849682d127303a2a06ae01d82f0e201
Dilean Barroso <dileanbarroso@gmail.com>
2017-09-14 03:43:14 -0400

    Update zinc.logger.js

--- bin/zinc.logger.js
+++ bin/zinc.logger.js
@@ -10,27 +10,35 @@ var cnGet = [srv + '[GET] API Works! ' + chalk.green('╭∩(^_^)'), srv + '[GET
 var cnPut = [srv + '[POST] ', srv + '[UPDATE] ']
 var cnDel = [srv + '[DELETE] Server has Deleted']
 
-var cnMongo = [srv + statusScc + 'Mongoose connected succesfully \n', srv + 'Mongoose Listening at ' + config.databaseURI + '\n']
+var cnMongo = [
+    srv + statusScc + 'Mongoose connected succesfully \n', 
+    srv + 'Mongoose Listening at ' + config.databaseURI + '\n',
+    srv + statusErr + "Failed to Connect to Mongo DB! Exiting . . . "
+]
 var cnZinc = [srv + statusScc + 'Zinc started sucessfully \n', srv + 'Listening on: ' + config.serverURI + ' . . . \n', srv + 'Zinc Web API is up at ' + config.serverURI + '/api \n']
 
 // System Logging Funtions
 function svCheck() { 
-    term(cnZinc[0] + cnZinc[2] + cnZinc[1]); 
+    console.log('============================')
+    console.log(' ZINC-CRM - PreAlpha v0.0.5 ') 
+    console.log('============================')
+    term(cnZinc[0] + cnZinc[2] + cnZinc[1])
 }
 function dbCheck() { 
     term(cnMongo[0]) 
 }
 
 // Error Handling Middleware
-function errMgmt(err) {
-    if (err.name || "ValidationError:") {
-        term(srv).red(err.name)(" ==> " + err.message + '\n')
-    } else { term(srv).red("Oop's, Something broke...") }
+function errMgmt(err) { 
+    term(srv).red(err.name + ': ')( err.message + '\n') 
+}
+function errFatal(err) { 
+    term(srv).red("Server failed to Connecto to Database!\n" + "   SERVER IS SHUTTING DOWN . . .\n")
 }
 
-console.log('============================' + '\n' + ' ZINC-CRM - PreAlpha v0.0.5 ' + '\n' + '============================')
+
 
 module.exports = {
-    errMgmt,
+    errMgmt, errFatal,
     srv, statusErr, statusScc, svCheck, dbCheck, cnGet, cnPut, cnDel, cnMongo, cnZinc
 }
